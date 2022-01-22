d79cd70758e39f5afd3690ae7c273b3c510ccce6
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-01-22 22:36:46 +0100

    used dark theme for the app with a little customization

---
+++ lib/price_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PriceScreen extends StatefulWidget {
+  const PriceScreen({Key? key}) : super(key: key);
+
+  @override
+  _PriceScreenState createState() => _PriceScreenState();
+}
+
+class _PriceScreenState extends State<PriceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('🤑 Coin Ticker'),
+      ),
+    );
+  }
+}
